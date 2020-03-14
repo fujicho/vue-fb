606a02ee66f0df567dddc2e29b5831a7562289cb
fujicho <nnnn.sohei.fujita@gmail.com>
2020-03-14 13:15:58 +0900

    データ表示

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,18 @@
   <div id="app">
     <h3>掲示板に投稿する</h3>
     <label for="name">ニックネーム</label>
-    <iput id="name" type="text" v-model="name"></iput>
+    <input id="name" type="text" v-model="name">
     <br><br>
     <label for="comment">コメント:</label>
     <textarea id="comment" v-model="comment"></textarea>
     <br><br>
     <button @click="createComment">コメントをサーバーに送る</button>
     <h2>掲示板</h2>
+    <div v-for="post in posts" :key="post.name">
+      <br>
+      <div>名前:{{post.fields.stringValue}}</div>
+      <div>コメント:{{post.fields.comment.stringValue}}</div>
+    </div>
   </div>
 </template>
 
@@ -18,14 +23,36 @@ export default {
   data() {
     return {
       name: "",
-      comment: ""
+      comment: "",
+      posts: []
     };
+  },
+  created(){
+    axios.get('https://firestore.googleapis.com/v1/projects/vuejs-http-92632/databases/(default)/documents/comments',
+    )
+    .then(response =>{
+      this.posts = response.data.documents
+      console.log(response);
+    })
   },
   methods:{
     createComment(){
-      axios.post('https://firestore.googleapis.com/v1/projects/vuejs-http-92632/databases/(default)/documents/comments)';
-    }
+      axios.post('https://firestore.googleapis.com/v1/projects/vuejs-http-92632/databases/(default)/documents/comments',
+      {
+        fields: {
+          name: {
+            stringValue:this.name
+          },
+          comment:{
+            stringValue: this.comment
+          }
+        }
+      }
+    )
+    this.name="";
+    this.comment="";
   }
+}
 }
 </script>
 
