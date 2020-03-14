9bcc3e0310dee26b0b4c398a4a3688af3be837d0
fujicho <nnnn.sohei.fujita@gmail.com>
2020-03-14 13:44:15 +0900

    requestとresponseの記述

--- src/main.js
+++ src/main.js
@@ -5,7 +5,26 @@ import axios from "axios";
 Vue.config.productionTip = false
 
 axios.defaults.baseURL = "https://firestore.googleapis.com/v1/projects/vuejs-http-92632/databases/(default)/documents"
-
+// サーバーに送る前の処理
+axios.defaults.request.use(
+  config =>{
+    console.log("interseptors request",config);
+    return config;
+  },
+  error =>{
+    return Promise.reject(error)
+  }
+);
+// サーバーから取ってきた時の処理
+axios.defaults.response.use(
+  response =>{
+    console.log("interseptors response",response);
+    return response;
+  },
+  error =>{
+    return Promise.reject(error)
+  }
+)
 
 new Vue({
   render: h => h(App),
