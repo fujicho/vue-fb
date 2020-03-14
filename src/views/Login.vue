0e65b3dcf1e601ccdcecad28327d3a634dc7f795
fujicho <nnnn.sohei.fujita@gmail.com>
2020-03-14 19:13:06 +0900

    簡易的なビューの作成

---
+++ src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>ログイン</h2>
+    <label for="email">Email:</label>
+    <input id="email" type="text" v-model="email">
+    <br><br>
+    <label for="password">パスワード:</label>
+    <input id="password" type="password" v-model="password">
+    <br><br>
+    <button @click="login">送信</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      comment: "",
+    };
+  },
+  methods:{
+    login(){
+
+    }
+  }
+}
+</script>
---
+++ src/views/Register.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>登録</h2>
+    <label for="email">Email:</label>
+    <input id="email" type="text" v-model="email">
+    <br><br>
+    <label for="password">パスワード:</label>
+    <input id="password" type="password" v-model="password">
+    <br><br>
+    <button @click="register">送信</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      comment: "",
+    };
+  },
+  methods:{
+    register(){
+      
+    }
+  }
+}
+</script>
