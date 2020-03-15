a080dffcffc6e6d3537e48a955d4af89acbe67d4
fujicho <nnnn.sohei.fujita@gmail.com>
2020-03-15 13:37:39 +0900

    vuexの導入

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import axios from "axios";
 import router from './router';
+import store from './store'
 
 Vue.config.productionTip = false
 
@@ -30,5 +31,6 @@ axios.interceptors.response.eject(interceptorsResponse);
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
